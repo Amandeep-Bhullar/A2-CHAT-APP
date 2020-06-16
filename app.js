a0ca69fab1e5a26cfd614e97940ca23566222ade
Amandeep-Bhullar <er.amandeepbhullar@gmail.com>
2020-06-15 23:05:39 -0400

    updated with time

--- app.js
+++ app.js
@@ -6,16 +6,29 @@ const io = require('socket.io')(3000);
 const app = express();
 
 const users={}
-
+/*
+    user {
+        id1: "Aman"
+    }
+*/
+// acts as a listner when connected
 io.on('connection', socket => {
-    socket.on('new-user', name=>{
+    // socket.on receives the message from client
+    socket.on('new-user', name =>{
+        // Store the user name  with socketID as key
         users[socket.id]=name
+        // send to every client
         socket.broadcast.emit('user-connected', name)
     })
-    socket.on('send-chat-message', message =>{
-        socket.broadcast.emit('chat-message',{message: message, name: 
-        users[socket.id]} )
-        console.log(message)
+    // catch a chat message
+    socket.on('send-chat-message', msg =>{
+        const obj = JSON.parse(msg);
+        const message = obj.message;
+        const time = obj.time;
+        console.log(obj);
+        socket.broadcast.emit('chat-message',
+            {   message: message, name: users[socket.id], time: time} 
+        )
     })
     socket.on('disconneted', () =>{
         socket.broadcast.emit('user-disconneted', users[socket.id])
--- public/js/script.js
+++ public/js/script.js
@@ -2,31 +2,64 @@ const socket = io('http://localhost:3000')
 const messageContainer= document.getElementById('messageContainer')
 const messageForm= document.getElementById('sendContainer')
 const messageInput= document.getElementById('messageInput')
-const name= prompt('What is your name?')
+const alertInp= prompt('What is your name?');
+
+const name = alertInp == null ? "Aman" : alertInp;
+//
 appendMessage('You joined')
 socket.emit('new-user', name)
 
+// msg sent from server is catched here
 socket.on('chat-message', data=>{
-    appendMessage(`${data.name}: ${data.message}`)
+    // Now msg is appended on container by calling appendMessage method
+    const innerObj = JSON.parse(data.message);
+    const msg = innerObj.message;
+    const time = innerObj.time;
+    appendMessage(`${data.name}: ${msg}`, time)
 })
+
 socket.on('user-connected',name =>{
     if(name)
         appendMessage(`${name} connected`)
 }) 
+
 socket.on('user-disconnected',name =>{
     appendMessage(`${name} disconnected`)
 })
 
+
 messageForm.addEventListener('submit',ele =>{
+    // stop the Page from reload
     ele.preventDefault()
     const message= messageInput.value;
-    appendMessage(`You: ${message}`)
-    socket.emit('send-chat-message',message)
+    const messageTime = new Date().toLocaleTimeString();
+    appendMessage(`You: ${message}`, messageTime)
+    // send the msg to server
+    socket.emit('send-chat-message', JSON.stringify({message:message, time: messageTime}))
+    // clear the input msg
     messageInput.value=''
 })
 
-function appendMessage(message){
-    const messageElement= document.createElement('div')
-    messageElement.innerText= message
-    messageContainer.append(messageElement)
+function appendMessage(message, time = null){
+    console.log(message);
+    if(time == null){
+        messageContainer.innerHTML += `
+        <div class="center-msg">
+            <span>${message}</span>
+        </div>`
+    }
+    else if(message.startsWith("You:")){
+        messageContainer.innerHTML += `
+        <div class="right-msg">
+            <span class="message">${message}</span>
+            <span class="time">${time}</span>
+        </div>`
+    }
+    else{
+        messageContainer.innerHTML += `
+        <div class="left-msg"> 
+            <span>${message}</span>
+            <span class="time">${time}</span>
+        </div>`
+    }
 }
